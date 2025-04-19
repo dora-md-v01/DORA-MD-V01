d76f6cb820df68cec3619fdb51a824156e6cfa93
dora-md-v01 <ircdora6@gmail.com>
2025-04-19 23:26:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ module.exports = {
     AUTO_VOICE: process.env.AUTO_VOICE || "false",
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     AUTHOR: (process.env.PACK_INFO?.split(';') || [])[0] || '🧑‍💻⃝𝘿𝙊𝙍𝘼 𝙈𝘿 𝙏𝙀𝘾𝙃➝💥',
-    PACKNAME: (process.env.PACK_INFO?.split(';') || [])[1] || '94740482244',
+    PACKNAME: (process.env.PACK_INFO?.split(';') || [])[1] || '94743454928',
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/x04f5n.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M DORA-MD WHATSAPP BOT 😊♻️",
