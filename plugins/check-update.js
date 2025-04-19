3877a9c955bbeaf3ee07a583a9151b49d7a2bff8
dora-md-v01 <ircdora6@gmail.com>
2025-04-19 23:34:58 +0530

    Update check-update.js

--- plugins/check-update.js
+++ plugins/check-update.js
@@ -53,7 +53,7 @@ cmd({
     const lastUpdate = fs.statSync(localVersionPath).mtime.toLocaleString();
 
     // GitHub stats
-    const githubRepo = 'https://github.com/LUCIFER-MD/LUCIFER-MD-V1';
+    const githubRepo = 'https://github.com/dora-md-v01/DORA-MD-V01';
 
     // Check update status
     let updateMessage = `✅ Your DORA-MD bot is up-to-date!`;
@@ -69,7 +69,7 @@ Use *.update* to update.`;
       `📌 *Bot Name:* DORA-MD\n🔖 *Current Version:* ${localVersion}\n📢 *Latest Version:* ${latestVersion}\n📂 *Total Plugins:* ${pluginCount}\n🔢 *Total Commands:* ${totalCommands}\n\n` +
       `💾 *System Info:*\n⏳ *Uptime:* ${uptime}\n📟 *RAM Usage:* ${ramUsage}MB / ${totalRam}MB\n⚙️ *Host Name:* ${hostName}\n📅 *Last Update:* ${lastUpdate}\n\n` +
       `📝 *Changelog:*\n${latestChangelog}\n\n` +
-      `⭐ *GitHub Repo:* ${githubRepo}\n👤 *Owner:* [𓆩𝙍𝙀𝘼𝙇 𝘿𝙊𝙍𝘼𓆪](https://github.com/LUCIFER-MD/LUCIFER-MD-V1)\n\n${updateMessage}\n\n🚀 *Hey! Don't forget to fork & star the repo!*`;
+      `⭐ *GitHub Repo:* ${githubRepo}\n👤 *Owner:* [𓆩𝙍𝙀𝘼𝙇 𝘿𝙊𝙍𝘼𓆪](https://github.com/dora-md-v01/DORA-MD-V01)\n\n${updateMessage}\n\n🚀 *Hey! Don't forget to fork & star the repo!*`;
 
     // Send the status message with an image
     await conn.sendMessage(from, {
