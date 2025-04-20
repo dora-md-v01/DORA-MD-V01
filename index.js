b4b6ce8cc02a6c606d1b6624862e462c7cf9b469
dora-md-v01 <ircdora6@gmail.com>
2025-04-20 17:05:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ const port = process.env.PORT || 9090;
   if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTOLIKESTATUS === "true") {
     const user = await conn.decodeJid(conn.user.id);
     await conn.sendMessage(mek.key.remoteJid,
-    { react: { key: mek.key, text: '💚' } },
+    { react: { key: mek.key, text: '🥀' } },
     { statusJidList: [mek.key.participant, user] }
     )};
     await Promise.all([
